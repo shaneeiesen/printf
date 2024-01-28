2882b7665b7efc1ac0cac3a0089bbf59ba192f26
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-28 21:05:52 +0200

    printf

---
+++ printf.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _print - function prototype
+ *
+ * @format: arguments count
+ * Return: 0
+ */
+
+int _printf(const char *format, ...) {
+    int count = 0;
+    va_list args;
+
+    va_start(args, format);
+
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c': {
+                    char c = va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *str = va_arg(args, char *);
+                    while (*str) {
+                        putchar(*str++);
+                        count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    count++;
+                    break;
+                }
+                default: {
+                    // Handle invalid conversion specifiers
+                    putchar(*format);
+                    count++;
+                    break;
+                }
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
