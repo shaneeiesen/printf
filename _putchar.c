2f0ffd83f0dc3bdb9cdd3b183662979ae74503cd
shaneeiesen <shanetech.it@gmail.com>
2024-01-28 18:00:37 -0800

    0x11. C - Printf

--- _putchar.c
+++ _putchar.c
@@ -4,10 +4,9 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 'count'.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (count);
 }
