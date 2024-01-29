33f9b6daa7835374b746f13f975cfd0e36ba7136
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-29 06:46:57 +0200

    printf

--- printf.c
+++ printf.c
@@ -1,59 +1,105 @@
-#include "main.h"
-
-/**
- * _print - function prototype
- *
- * @format: arguments count
- * Return: 0
- */
-
-int _printf(const char *format, ...)
-{
-        unsigned int count = 0;
-        va_list args;
-
-        va_start(args, format);
-
-        while (*format != '\0')
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        switch (*format)
-                        {
-                                case 'c':
-                                {
-                                        char c = va_arg(args, int);
-
-                                        putchar(c);
-                                        count++;
-                                        break;
-                                }
-                                case 's':
-                                {
-                                        char *str = va_arg(args, char*);
-
-                                        if (str != NULL)
-                                        {
-                                                while (*str)
-                                        {
-                                                        putchar(*str++);
-                                                        count++;
-                                        }
-                                        }
-                                        break;
-                                }
-                                case '%':
-                                {
-
-                                        putchar('%');
-                                        count++;
-                                        break;
-                                }
-                                default:
-                                        putchar('%');
-                                        putchar(*format);
-                                        count += 2;
-printf.c [dos] (06:28 29/01/2024)                                                                                                                                                                     34,12-47 Top
-"printf.c" [dos] 69L, 921B
-
+#include <stdarg.h> 
+#include <stdio.h> 
+   
+int myprintf(const char* str, ...) 
+{ 
+    // initializing list pointer 
+    va_list ptr; 
+    va_start(ptr, str); 
+  
+    // char array to store token 
+    char token[1000]; 
+    // index of where to store the characters of str in 
+    // token 
+    int k = 0; 
+  
+    // parsing the formatted string 
+    for (int i = 0; str[i] != '\0'; i++) { 
+        token[k++] = str[i]; 
+  
+        if (str[i + 1] == '%' || str[i + 1] == '\0') { 
+            token[k] = '\0'; 
+            k = 0; 
+            if (token[0] != '%') { 
+                fprintf( 
+                    stdout, "%s", 
+                    token); // printing the whole token if 
+                            // it is not a format specifier 
+            } 
+            else { 
+                int j = 1; 
+                char ch1 = 0; 
+  
+                // this loop is required when printing 
+                // formatted value like 0.2f, when ch1='f' 
+                // loop ends 
+                while ((ch1 = token[j++]) < 58) { 
+                } 
+                // for integers 
+                if (ch1 == 'i' || ch1 == 'd' || ch1 == 'u'
+                    || ch1 == 'h') { 
+                    fprintf(stdout, token, 
+                            va_arg(ptr, int)); 
+                } 
+                // for characters 
+                else if (ch1 == 'c') { 
+                    fprintf(stdout, token, 
+                            va_arg(ptr, int)); 
+                } 
+                // for float values 
+                else if (ch1 == 'f') { 
+                    fprintf(stdout, token, 
+                            va_arg(ptr, double)); 
+                } 
+                else if (ch1 == 'l') { 
+                    char ch2 = token[2]; 
+  
+                    // for long int 
+                    if (ch2 == 'u' || ch2 == 'd'
+                        || ch2 == 'i') { 
+                        fprintf(stdout, token, 
+                                va_arg(ptr, long)); 
+                    } 
+  
+                    // for double 
+                    else if (ch2 == 'f') { 
+                        fprintf(stdout, token, 
+                                va_arg(ptr, double)); 
+                    } 
+                } 
+                else if (ch1 == 'L') { 
+                    char ch2 = token[2]; 
+  
+                    // for long long int 
+                    if (ch2 == 'u' || ch2 == 'd'
+                        || ch2 == 'i') { 
+                        fprintf(stdout, token, 
+                                va_arg(ptr, long long)); 
+                    } 
+  
+                    // for long double 
+                    else if (ch2 == 'f') { 
+                        fprintf(stdout, token, 
+                                va_arg(ptr, long double)); 
+                    } 
+                } 
+  
+                // for strings 
+                else if (ch1 == 's') { 
+                    fprintf(stdout, token, 
+                            va_arg(ptr, char*)); 
+                } 
+  
+                // print the whole token 
+                // if no case is matched 
+                else { 
+                    fprintf(stdout, "%s", token); 
+                } 
+            } 
+        } 
+    } 
+  
+    // ending traversal 
+    va_end(ptr); 
+    return 0; 
+}
