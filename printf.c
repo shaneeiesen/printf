ae2920d64605392b7050b190b17d18a888e4b68d
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-29 06:41:18 +0200

    printf

---
+++ printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * _print - function prototype
+ *
+ * @format: arguments count
+ * Return: 0
+ */
+
+int _printf(const char *format, ...)
+{
+        unsigned int count = 0;
+        va_list args;
+
+        va_start(args, format);
+
+        while (*format != '\0')
+        {
+                if (*format == '%')
+                {
+                        format++;
+                        switch (*format)
+                        {
+                                case 'c':
+                                {
+                                        char c = va_arg(args, int);
+
+                                        putchar(c);
+                                        count++;
+                                        break;
+                                }
+                                case 's':
+                                {
+                                        char *str = va_arg(args, char*);
+
+                                        if (str != NULL)
+                                        {
+                                                while (*str)
+                                        {
+                                                        putchar(*str++);
+                                                        count++;
+                                        }
+                                        }
+                                        break;
+                                }
+                                case '%':
+                                {
+
+                                        putchar('%');
+                                        count++;
+                                        break;
+                                }
+                                default:
+                                        putchar('%');
+                                        putchar(*format);
+                                        count += 2;
+printf.c [dos] (06:28 29/01/2024)                                                                                                                                                                     34,12-47 Top
+"printf.c" [dos] 69L, 921B
+
