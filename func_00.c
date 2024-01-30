4471fef5018ae0e8b55232f4e020056e1dc4a2db
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-30 19:36:02 +0200

    func_00.c

--- func_00.c
+++ func_00.c
@@ -81,7 +81,7 @@ int print_non_printable(va_list args, char buffer[],
 		if (is_printable(input_str[str_index]))
 			buffer[str_index + buffer_offset] = input_str[str_index];
 		else
-			buffer_offset += append_hexa_code(input_str[str_index],\
+			buffer_offset += append_hexa_code(input_str[str_index], 
 					buffer, str_index + buffer_offset);
 		str_index++;
 	}
@@ -150,9 +150,9 @@ int print_rot13string(va_list args, char buffer[],
 	char *input_str;
 	unsigned int i, j;
 	int output_count = 0;
-	char normal_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+	char normal_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ
 		abcdefghijklmnopqrstuvwxyz";
-	char rot13_alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLM\
+	char rot13_alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLM
 		nopqrstuvwxyzabcdefghijklm";
 
 	input_str = va_arg(args, char *);
