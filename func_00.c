3267b52cb853fc44504ba2cda3872c62811ad012
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-30 19:26:43 +0200

    func_00.c

--- func_00.c
+++ func_00.c
@@ -12,43 +12,42 @@
  * Return: Number of chars printed.
  */
 int print_pointer(va_list args, char buffer[],
-                  int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-    char sign_char = 0, padding_char = ' ';
-    int buffer_index = BUFF_SIZE - 2, output_length = 2; // length=2 for '0x'
-    unsigned long address_value;
-    char hex_digits[] = "0123456789abcdef";
-    void *address = va_arg(args, void *);
+	char sign_char = 0, padding_char = ' ';
+	int buffer_index = BUFF_SIZE - 2, output_length = 2;
+	unsigned long address_value;
+	char hex_dIgits[] = "0123456789abcdef";
+	void *address = va_arg(args, void *);
 
-    UNUSED(width);
-    UNUSED(size);
+	UNUSED(width);
+	UNUSED(size);
 
-    if (address == NULL)
-        return (write(1, "(nil)", 5));
+	if (address == NULL)
+		return (write(1, "(nil)", 5));
 
-    buffer[BUFF_SIZE - 1] = '\0';
-    UNUSED(precision);
+	buffer[BUFF_SIZE - 1] = '\0';
+	UNUSED(precision);
 
-    address_value = (unsigned long)address;
+	address_value = (unsigned long)address;
 
-    while (address_value > 0)
-    {
-        buffer[buffer_index--] = hex_digits[address_value % 16];
-        address_value /= 16;
-        output_length++;
-    }
+	while (address_value > 0)
+	{
+		buffer[buffer_index--] = hex_digits[address_value % 16];
+		address_value /= 16;
+		output_length++;
+	}
 
-    if ((flags & F_ZERO) && !(flags & F_MINUS))
-        padding_char = '0';
-    if (flags & F_PLUS)
-        sign_char = '+', output_length++;
-    else if (flags & F_SPACE)
-        sign_char = ' ', output_length++;
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padding_char = '0';
+	if (flags & F_PLUS)
+		sign_char = '+', output_length++;
+	else if (flags & F_SPACE)
+		sign_char = ' ', output_length++;
 
-    buffer_index++;
-
-    return (write_pointer(buffer, buffer_index, output_length,
-                          width, flags, padding_char, sign_char, 1));
+	buffer_index++;
+	return (write_pointer(buffer, buffer_index, output_length,
+				width, flags, padding_char, sign_char, 1));
 }
 
 /************************* PRINT NON PRINTABLE *************************/
@@ -62,33 +61,32 @@ int print_pointer(va_list args, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
 int print_non_printable(va_list args, char buffer[],
-                        int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-    int str_index = 0, buffer_offset = 0;
-    char *input_str = va_arg(args, char *);
+	int str_index = 0, buffer_offset = 0;
+	char *input_str = va_arg(args, char *);
 
-    UNUSED(flags);
-    UNUSED(width);
-    UNUSED(precision);
-    UNUSED(size);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
 
-    if (input_str == NULL)
-        return (write(1, "(null)", 6));
+	if (input_str == NULL)
+		return (write(1, "(null)", 6));
 
-    while (input_str[str_index] != '\0')
-    {
-        if (is_printable(input_str[str_index]))
-            buffer[str_index + buffer_offset] = input_str[str_index];
-        else
-            buffer_offset += append_hexa_code(input_str[str_index], buffer, str_index + buffer_offset);
+	while (input_str[str_index] != '\0')
+	{
+		if (is_printable(input_str[str_index]))
+			buffer[str_index + buffer_offset] = input_str[str_index];
+		else
+			buffer_offset += append_hexa_code(input_str[str_index], buffer, str_index + buffer_offset);
+		str_index++;
+	}
 
-        str_index++;
-    }
-
-    buffer[str_index + buffer_offset] = '\0';
-
-    return (write(1, buffer, str_index + buffer_offset));
+	buffer[str_index + buffer_offset] = '\0';
+	return (write(1, buffer, str_index + buffer_offset));
 }
 
 /************************* PRINT REVERSE *************************/
@@ -103,34 +101,33 @@ int print_non_printable(va_list args, char buffer[],
  * Return: Numbers of chars printed
  */
 int print_reverse(va_list args, char buffer[],
-                  int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-    char *str;
-    int str_len, char_count = 0;
+	char *str;
+	int str_len, char_count = 0;
+	
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
 
-    UNUSED(buffer);
-    UNUSED(flags);
-    UNUSED(width);
-    UNUSED(size);
+	str = va_arg(args, char *);
 
-    str = va_arg(args, char *);
+	if (str == NULL)
+	{
+		UNUSED(precision);
+		str = ")Null(";
+	}
 
-    if (str == NULL)
-    {
-        UNUSED(precision);
-        str = ")Null(";
-    }
-
-    for (str_len = 0; str[str_len]; str_len++)
-        ;
-
-    for (str_len = str_len - 1; str_len >= 0; str_len--)
-    {
-        char current_char = str[str_len];
-        write(1, &current_char, 1);
-        char_count++;
-    }
-    return (char_count);
+	for (str_len = 0; str[str_len]; str_len++)
+		;
+	for (str_len = str_len - 1; str_len >= 0; str_len--)
+	{
+		char current_char = str[str_len];
+		write(1, &current_char, 1);
+		char_count++;
+	}
+	return (char_count);
 }
 
 /************************* PRINT A STRING IN ROT13 *************************/
@@ -145,43 +142,42 @@ int print_reverse(va_list args, char buffer[],
  * Return: Numbers of chars printed
  */
 int print_rot13string(va_list args, char buffer[],
-                      int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-    char transformed_char;
-    char *input_str;
-    unsigned int i, j;
-    int output_count = 0;
-    char normal_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char rot13_alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char transformed_char;
+	char *input_str;
+	unsigned int i, j;
+	int output_count = 0;
+	char normal_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13_alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    input_str = va_arg(args, char *);
-    UNUSED(buffer);
-    UNUSED(flags);
-    UNUSED(width);
-    UNUSED(precision);
-    UNUSED(size);
+	input_str = va_arg(args, char *);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
 
-    if (input_str == NULL)
-        input_str = "(AHYY)";
-    for (i = 0; input_str[i]; i++)
-    {
-        for (j = 0; normal_alphabet[j]; j++)
-        {
-            if (normal_alphabet[j] == input_str[i])
-            {
-                transformed_char = rot13_alphabet[j];
-                write(1, &transformed_char, 1);
-                output_count++;
-                break;
-            }
-        }
-        if (!normal_alphabet[j])
-        {
-            transformed_char = input_str[i];
-            write(1, &transformed_char, 1);
-            output_count++;
-        }
-    }
-    return (output_count);
+	if (input_str == NULL)
+		input_str = "(AHYY)";
+	for (i = 0; input_str[i]; i++)
+	{
+		for (j = 0; normal_alphabet[j]; j++)
+		{
+			if (normal_alphabet[j] == input_str[i])
+			{
+				transformed_char = rot13_alphabet[j];
+				write(1, &transformed_char, 1);
+				output_count++;
+				break;
+			}
+		}
+		if (!normal_alphabet[j])
+		{
+			transformed_char = input_str[i];
+			write(1, &transformed_char, 1);
+			output_count++;
+		}
+	}
+	return (output_count);
 }
-
